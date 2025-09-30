1455902965a070bab3e1c75852f02d08948116ad
Sascha Brückl <sas@Mac.home>
2025-09-30 21:53:30 +0200

    changed baseURL

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "quartz.dreamwaver.at",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
